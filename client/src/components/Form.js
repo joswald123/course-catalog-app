7057978336f4d3a73178a084a5406b607726e699
joswald123 <julianaandreaoswald@gmail.com>
2022-08-06 20:25:07 -0700

    add styles form error

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -41,9 +41,9 @@ function ErrorsDisplay({ errors }) {
 
   if (errors.length) {
     errorsDisplay = (
-      <div className="wrap">
-        <h3>Validation errors</h3>
+      <div>
         <div className="validation--errors">
+          <h3>Validation errors</h3>
           <ul>
             {errors.map((error, i) => <li key={i}>{error}</li>)}
           </ul>
